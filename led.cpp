0d2c9f05c0178ed8cf97c745a6585b85687dcfda
unknown <kpiziak@student.agh.edu.pl>
2026-03-24 21:20:34 +0100

    8b sim ok

--- led.cpp
+++ led.cpp
@@ -17,25 +17,29 @@ void Led::On(unsigned char ucLedIndex)
 {
 	IO1CLR = (LED0_bm | LED1_bm | LED2_bm | LED3_bm);
 	
+	unsigned int LedBitMask;
+	
 	switch(ucLedIndex){
 		case 0:
-			IO1SET = LED0_bm;
+			LedBitMask = LED0_bm;
 			break;
 		case 1:
-			IO1SET = LED1_bm; 
+			LedBitMask = LED1_bm; 
 			break;
 		case 2:
-			IO1SET = LED2_bm; 
+			LedBitMask = LED2_bm; 
 			break;
 		case 3:
-			IO1SET = LED3_bm; 
+			LedBitMask = LED3_bm; 
 			break;
 		default:
 			break;		
 	}
 	if(ucInversion){
-		IO1SET = (~IO1SET & (0xF<<16));
+		LedBitMask = LedBitMask ^ (0xF<<16);
 		}else{};
+	
+	IO1SET = LedBitMask;
 	//IO1SET = (ucInversion) ? ~IO1SET : IO1SET;
 
 }	
